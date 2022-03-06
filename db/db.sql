7df10187a748c2f1f40cf36f56d7a6fc5fb56264
Carlo Sganzerla <carlosganzrl@hotmail.com>
2022-03-06 13:07:53 -0300

    Added docker compose and DB

---
+++ db/db.sql
@@ -0,0 +1,34 @@
+CREATE EXTENSION IF NOT EXISTS pgplsql;
+CREATE EXTENSION IF NOT EXISTS uuid-ossp;
+
+CREATE TABLE queue (
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    payload jsonb NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    visible_at timestamptz NOT NULL DEFAULT NOW(),
+    retry_count integer NOT NULL DEFAULT 0
+);
+
+
+WITH next_message AS (
+    SELECT
+        *
+    FROM
+        queue
+    WHERE
+        visible_at <= NOW()
+    ORDER BY
+        created_at
+    LIMIT 1
+    FOR SHARE
+)
+UPDATE
+    queue
+SET
+    visible_at = NOW() + INTERVAL '300 SECONDS'
+FROM
+    next_message
+WHERE
+    queue.id = next_message.id
+RETURNING
+    queue.*;
